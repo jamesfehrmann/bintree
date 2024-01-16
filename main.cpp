a8ed0851c1d1c83861973c2c695289b6cc48543b
James Fehrmann <jfehrmann@outlook.com>
2024-01-16 15:46:59 -0500

    quick commit

--- main.cpp
+++ main.cpp
@@ -2,33 +2,62 @@
 // Simple Binary Tree Walker
 // License: MIT
 // Editor: Neovim
-// Date: 01/17/24
+// Date: 01/16/24
 
-#include <chrono>
 #include <iostream>
-using namespace std::chrono;
 
-time_point<high_resolution_clock> now() { return high_resolution_clock::now(); }
+struct TreeNode {
+    int value;
+    TreeNode *left, *right;
+    TreeNode(int nodeValue, TreeNode *left, TreeNode *right) : value(nodeValue), left(left), right(right) {}
+};
 
-float getDeltaTime(time_point<high_resolution_clock> lastTime) {
-    return duration_cast<duration<float>>(now() - lastTime).count();
-}
-
-time_point<high_resolution_clock> updateLastTime() { return now(); }
-
-// time sink
-void sleep(int ms) {
-    float z = ms / 1000.f;
-    time_point<high_resolution_clock> lastTime = updateLastTime();
-    while (getDeltaTime(lastTime) < z) {
-        // grab a snickers
+TreeNode *treeBuilder(int i) {
+    if (i == 0) {
+        return new TreeNode(0, nullptr, nullptr);
     }
+    return new TreeNode(i, treeBuilder(i - 1), treeBuilder(i - 1));
 }
 
+enum WalkMethod { INORDER,
+                  PREORDER,
+                  POSTORDER };
 
+void printNode(TreeNode *node) {
+    std::cout << node->value << " " << std::flush;
+}
 
-void printBreak() { std::cout << "-----------------------" << std::endl; }
+void treeWalk(TreeNode *node, WalkMethod w) {
+    if (w == WalkMethod::PREORDER)
+        printNode(node);
+    if (node->value > 0)
+        treeWalk(node->left, w);
+    if (w == WalkMethod::INORDER)
+        printNode(node);
+    if (node->value > 0)
+        treeWalk(node->right, w);
+    if (w == WalkMethod::POSTORDER)
+        printNode(node);
+}
 
 int main() {
+    std::cout << "---" << std::endl;
+
+    int h = 4;
+    TreeNode *tree = treeBuilder(h);
+
+    std::cout << "Inorder   Traversal: ";
+    treeWalk(tree, WalkMethod::INORDER);
+    std::cout << std::endl;
+
+    std::cout << "Preorder  Traversal: ";
+    treeWalk(tree, WalkMethod::PREORDER);
+    std::cout << std::endl;
+
+    std::cout << "Postorder Traversal: ";
+    treeWalk(tree, WalkMethod::POSTORDER);
+    std::cout << std::endl;
+
+    std::cout << "---" << std::endl;
     return 0;
 }
